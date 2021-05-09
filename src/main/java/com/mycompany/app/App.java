e26a3d77030e33bc6bbb1b28fb293cdd1b3d2ac4
Ubuntu <kevin.murray.38@gmail.com>
2021-05-09 14:02:58 +0000

    change to make build fail

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -6,7 +6,7 @@ package com.mycompany.app;
 public class App
 {
 
-    private final String message = "Hello World! Changes to poll new build";
+    private final String message = "Hello World! Changes to poll new build"
 
     public App() {}
 
